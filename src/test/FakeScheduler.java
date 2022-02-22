1b898eac5e2f6a0275bbf0e196e798e43e0faeff
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 04:00:26 -0700

    Start endpoint is returning successfully!

--- src/test/FakeScheduler.java
+++ src/test/FakeScheduler.java
@@ -1,12 +1,22 @@
 package test;
 
+import main.SchedulerException;
+import main.response.StartResponse;
 import main.service.AppointmentService;
 import main.service.SchedulingAPIService;
 
 public class FakeScheduler {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SchedulerException {
         /* 0. Initialize main.service instances */
         SchedulingAPIService schedulingAPIService = new SchedulingAPIService();
         AppointmentService appointmentService = AppointmentService.getInstance();
+
+        /* 1. Start API */
+        StartResponse startResponse = SchedulingAPIService.startAPI();
+        if (!startResponse.isSuccess()) {
+            throw new SchedulerException(startResponse.getMessage());
+        }
+
+        System.out.println(startResponse.isSuccess());;
     }
 }
