7800f12f36f77d75238f9873e9b02a9a75e6ef9a
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 01:51:51 -0700

    General POST/GET methods added

--- src/ClientCommunicator.java
+++ src/ClientCommunicator.java
@@ -1,9 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 
 public class ClientCommunicator {
     private static final String SCHEDULER_API_URL = "http://scheduling-interview-2021-265534043.us-west-2.elb.amazonaws.com/api/Scheduling";
     private static final String AUTH_TOKEN = "3206d505-c34c-4aa8-be83-7dba807ea7c2";
 
-    public static HttpResponse<String> doPost(String endpointPath, String requestBody) {}
-    public static HttpResponse<String> doGet(String endpointPath) {}
+    public static HttpResponse<String> doPost(String endpointPath, String requestBody) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(SCHEDULER_API_URL + endpointPath + "?token=" + AUTH_TOKEN))
+                .POST(BodyPublishers.ofString(requestBody))
+                .header("Content-Type", "application/json")
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> doGet(String endpointPath) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(SCHEDULER_API_URL + endpointPath + "?token=" + AUTH_TOKEN))
+                .GET()
+                .header("Content-Type", "application/json")
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
 }
