4c26e70725e9b2eecee949c6550f37341674d586
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 02:27:11 -0700

    Fields + constructors + getters added

--- src/response/ScheduleAppointmentResponse.java
+++ src/response/ScheduleAppointmentResponse.java
@@ -1,4 +1,11 @@
 package response;
 
-public class ScheduleAppointmentResponse {
+public class ScheduleAppointmentResponse extends Response {
+    public ScheduleAppointmentResponse(boolean success) {
+        super(success, null);
+    }
+
+    public ScheduleAppointmentResponse(boolean success, String message) {
+        super(success, message);
+    }
 }
--- src/response/StopResponse.java
+++ src/response/StopResponse.java
@@ -1,4 +1,22 @@
 package response;
 
-public class StopResponse {
+import model.Appointment;
+
+import java.util.List;
+
+public class StopResponse extends Response {
+    private List<Appointment> appointments;
+
+    public StopResponse(List<Appointment> appointments) {
+        super(true);
+        this.appointments = appointments;
+    }
+
+    public StopResponse(String message) {
+        super(false, message);
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
 }
