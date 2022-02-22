ef14777a755e935af5aa49bda5d5e3206a06fa5f
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 03:23:52 -0700

    Added Singleton methods, fields, getters + setters

--- src/service/AppointmentService.java
+++ src/service/AppointmentService.java
@@ -1,4 +1,28 @@
 package service;
 
-public class ApointmentService {
+import model.Appointment;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppointmentService {
+    private static AppointmentService instance = null;
+    private ArrayList<Appointment> scheduledAppointments;
+    private ArrayList<ZonedDateTime> allTimeSlots;
+
+    public static AppointmentService getInstance() {
+        if (instance == null) {
+            instance = new AppointmentService();
+        }
+        return instance;
+    }
+
+    public void setScheduledAppointments(List<Appointment> appointments) {
+        this.scheduledAppointments = (ArrayList<Appointment>) appointments;
+    }
+
+    public List<Appointment> getScheduledAppointments() {
+        return this.scheduledAppointments;
+    }
 }
