2003eecc807bea54e0cd5643ce595d879b286c23
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 02:24:00 -0700

    Primitive wrappers added for null sets and checks (on 200 status but no requests left)

--- src/response/NextAppointmentResponse.java
+++ src/response/NextAppointmentResponse.java
@@ -4,11 +4,11 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 public class NextAppointmentResponse extends Response {
-    private int requestId;
-    private int personId;
+    private Integer requestId;
+    private Integer personId;
     private List<ZonedDateTime> preferredDays;
     private List<Integer> preferredDocs;
-    private boolean isNew;
+    private Boolean isNew;
 
     public NextAppointmentResponse(int requestId, int personId, List<ZonedDateTime> preferredDays, List<Integer> preferredDocs, boolean isNew) {
         super(true, null);
@@ -21,7 +21,11 @@ public class NextAppointmentResponse extends Response {
 
     public NextAppointmentResponse(boolean success, String message) {
         super(success, message);
-        this.requestId = -1; 
+        this.requestId = null;
+        this.personId = null;
+        this.preferredDays = null;
+        this.preferredDocs = null;
+        this.isNew = null;
     }
 
     public Integer getRequestId() {
