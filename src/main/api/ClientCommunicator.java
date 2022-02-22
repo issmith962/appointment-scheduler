ab46af29cdf2b5a24afd7f37ffd8e3f884a61b5d
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 04:00:03 -0700

    Added 5 sec timeout since api request was spinning

--- src/main/api/ClientCommunicator.java
+++ src/main/api/ClientCommunicator.java
@@ -6,15 +6,18 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class ClientCommunicator {
     private static final String SCHEDULER_API_URL = "http://scheduling-interview-2021-265534043.us-west-2.elb.amazonaws.com/api/Scheduling";
     private static final String AUTH_TOKEN = "3206d505-c34c-4aa8-be83-7dba807ea7c2";
+    private static final int TIMEOUT = 5;
 
     public static HttpResponse<String> doPost(String endpointPath, String requestBody) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(SCHEDULER_API_URL + endpointPath + "?token=" + AUTH_TOKEN))
+                .timeout(Duration.ofSeconds(TIMEOUT))
                 .POST(BodyPublishers.ofString(requestBody))
                 .header("Content-Type", "application/json")
                 .build();
@@ -25,6 +28,7 @@ public class ClientCommunicator {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(SCHEDULER_API_URL + endpointPath + "?token=" + AUTH_TOKEN))
+                .timeout(Duration.ofSeconds(TIMEOUT))
                 .GET()
                 .header("Content-Type", "application/json")
                 .build();
