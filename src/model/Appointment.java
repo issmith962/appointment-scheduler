42acf143cc0c1b8ff7eb06adb086b1b017b96e9d
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 02:15:36 -0700

    fields + constructor + getters added

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,4 +1,33 @@
 package model;
 
+import java.time.ZonedDateTime;
+
 public class Appointment {
+    private int doctorId;
+    private int personId;
+    private ZonedDateTime appointmentTime;
+    private boolean isNewPatientAppointment;
+
+    public Appointment(int doctorId, int personId, ZonedDateTime appointmentTime, boolean isNewPatientAppointment) {
+        this.doctorId = doctorId;
+        this.personId = personId;
+        this.appointmentTime = appointmentTime;
+        this.isNewPatientAppointment = isNewPatientAppointment;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public ZonedDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public boolean isNewPatientAppointment() {
+        return isNewPatientAppointment;
+    }
 }
