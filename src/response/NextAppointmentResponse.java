57dab97162d24bf217cf270afe4805d3a7782c53
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 02:22:46 -0700

    Fields + constructor + getters added

--- src/response/NextAppointmentResponse.java
+++ src/response/NextAppointmentResponse.java
@@ -1,4 +1,46 @@
 package response;
 
-public class NextAppointmentResponse {
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class NextAppointmentResponse extends Response {
+    private int requestId;
+    private int personId;
+    private List<ZonedDateTime> preferredDays;
+    private List<Integer> preferredDocs;
+    private boolean isNew;
+
+    public NextAppointmentResponse(int requestId, int personId, List<ZonedDateTime> preferredDays, List<Integer> preferredDocs, boolean isNew) {
+        super(true, null);
+        this.requestId = requestId;
+        this.personId = personId;
+        this.preferredDays = preferredDays;
+        this.preferredDocs = preferredDocs;
+        this.isNew = isNew;
+    }
+
+    public NextAppointmentResponse(boolean success, String message) {
+        super(success, message);
+        this.requestId = -1; 
+    }
+
+    public Integer getRequestId() {
+        return requestId;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public List<ZonedDateTime> getPreferredDays() {
+        return preferredDays;
+    }
+
+    public List<Integer> getPreferredDocs() {
+        return preferredDocs;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
 }
