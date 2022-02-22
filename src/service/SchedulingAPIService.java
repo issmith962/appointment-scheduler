9017c3f673a9f8b33dc7657fc607b88ff2f56ea8
Isaac Smith <is.smith.962@gmail.com>
2022-02-22 02:04:22 -0700

    Package rearranging

--- src/service/SchedulingAPIService.java
+++ src/service/SchedulingAPIService.java
@@ -1,16 +1,4 @@
 package service;
 
 public class SchedulingAPIService {
-    private static final String START_PATH = "/Start";
-    private static final String STOP_PATH = "/Stop";
-    private static final String APPOINTMENT_REQUEST_PATH = "/AppointmentRequest";
-    private static final String SCHEDULE_PATH = "/Schedule";
-
-    public SchedulingAPIService() {}
-
-    public static StartResponse startAPI() {}
-    public static StopResponse stopAPI() {}
-    public static NextAppointmentResponse getNextAppointmentRequest() {}
-    public static InitialScheduleResponse getInitialSchedule () {}
-    public static ScheduleAppointmentResponse scheduleAppointment(ScheduleAppointmentRequest request) {}
 }
